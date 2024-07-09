f99458676eb8fb5b4e1d8500a69a10ca173ece9f
Yaroslav Shkarbuta <Yaroslav@MacbookPro-Yaroslav-Sh.local>
2024-07-10 02:26:36 +0300

    9laba comments

--- Laba 9/main.c
+++ Laba 9/main.c
@@ -17,8 +17,9 @@ int minSteps(int x, int y) {
     return steps;
 }
 
+// ill use what u wrote as basis for checking the code
 int main() {
-    printf("%d\n", minSteps(45, 48)); // Should output: 3
+    printf("%d\n", minSteps(45, 48)); // Should output: 3 
     printf("%d\n", minSteps(45, 49)); // Should output: 3
     printf("%d\n", minSteps(45, 50)); // Should output: 4
     printf("%d\n", minSteps(45, 51)); // Should output: 4
